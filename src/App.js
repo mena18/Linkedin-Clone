4e7000b945fb6811369940b93d5345911b83ce2c
mena18 <menan381@gmail.com>
2021-07-12 23:57:29 +0200

    add react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Header</h1>
+      <div className="header_left">left header</div>
+
+      <div className="header_right">right header</div>
+    </div>
+  );
+}
